8ff2c65893bcba7e99c94781ba3e1259eaf12656
taisei899 <himesama1023@gmail.com>
2021-04-15 16:29:25 +0900

    3

--- 9_minutes_game/game.js
+++ 9_minutes_game/game.js
@@ -1,6 +1,13 @@
 window.onload=function(){
 
     var game = new enchant.Game(320,320);
+
+    game.preload("charal.gif");
+    game.onload = function(){
+    var bear =new enchant.Sprite(32,32);
+    bear.image = game.assets["charal.gif"];
+    game.rootScene.addChild(bear);
+    }
     game.start();
     game.rootScene.backgroundColor = "red";
 }
