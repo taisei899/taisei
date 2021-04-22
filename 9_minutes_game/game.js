bfb7f9095fa0ff972b6c90f839e95dadaefa91f0
taisei899 <himesama1023@gmail.com>
2021-04-22 14:27:05 +0900

    1

--- 9_minutes_game/game.js
+++ 9_minutes_game/game.js
@@ -1,29 +1,27 @@
-window.onload=function(){
+window.onload=function() {
 
-    var game = new enchant.Game(320,320);
+    var game = new Game(320,320);
 
     game.preload("charal.gif");
     game.onload = function(){
-    var bear =new enchant.Sprite(32,32);
+
+    var bear = new enchant.Sprite(32,32);
     bear.image = game.assets["charal.gif"];
     game.rootScene.addEvenListener("touchmove",function(e) {
         bear.x = e.x;
-        bear.y = e/y;
+        bear.y = e.y;
     });
+    game.rootScene.addEventListener("enterframe",function)() {
+    if(game.frame % 10 === 0){
     var enemy = new Sprite(32, 32);
     enemy.image = game.assets["charal.gif"];
     enemy.frame = 5;
-    
-    enemy.addEventListener("enterframe", function(){
-        if(game. frame % 10 === 0) {
-            var enemy = new Sprite(32, 32);
-        enemy.image = game.assets["charal.gif"];
-        enemy.frame = 5;
-        enemy.x = (320 - 32) * Math.random();
-        enemy.addEventListener("enterframe",function(){
-            enemy.y += 5;
-    
-    
+    enemy.x = (320 - 32) * Math.random();
+    enemy.addEventListener("enterframe",function(){
+    enemy.y += 5;
+    if(enemy.within(bear)) {
+        game.stop();
+    }
     });
     
     game.rootScene.addChild(enemy);
@@ -33,5 +31,4 @@ window.onload=function(){
 
     }
     game.start();
-    game.rootScene.backgroundColor = "red";
 }
